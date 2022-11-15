b4421f73b3f493b206171d5f17cc467f6af85816
puneet <puneetdhiman56@gmail.com>
2022-11-15 23:02:41 +0530

    cleanup

--- lua/user/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-
-local colorscheme = "murphy"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("Colorscheme " ..colorscheme .." not found")
-  return
-end
-
